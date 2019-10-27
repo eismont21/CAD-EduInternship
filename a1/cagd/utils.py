bd38036f5c79b7596f717b09b3c442b2a656e948
Aleksandr <alexander_eismont@mail.ru>
2019-10-27 18:20:51 +0100

    solve_tridiagonal_equation implemented

--- a1/cagd/utils.py
+++ a1/cagd/utils.py
@@ -9,7 +9,36 @@ from cagd.vec import vec2
 #therefore diag1[i], diag2[i] and diag3[i] are located on the same row of A
 def solve_tridiagonal_equation(diag1, diag2, diag3, res):
     assert(len(diag1) == len(diag2) == len(diag3) == len(res))
-    pass
+    n = len(diag2)
+    x = [0] * n
+    y = [0] * n
+    v = [0] * n
+    a = [0]
+    a.extend(diag1)
+    z = [0] * n
+    c = diag3
+    c.append(0)
+    b = diag2
+    d = res
+
+    i = 0
+    while i < n:
+        z[i] = 1 / (b[i] - a[i] * v[i])
+        if i != n - 1:
+            v[i + 1] = z[i] * c[i]
+        if i == 0:
+            y[i] = z[i] * d[i]
+        else:
+            y[i] = z[i] * (d[i] - a[i] * y[i - 1])
+        i = i + 1
+
+    x[n - 1] = y[n - 1]
+    i = n - 2
+    while i > -1:
+        x[i] = y[i] - v[i + 1] * x[i + 1]
+        i = i - 1
+
+    return x
      
 
 #solves the system of linear equations Ax = res
