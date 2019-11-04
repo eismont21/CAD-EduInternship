a9820930d0674ec585865f34cfdb6f52fca010c6
Aleksandr <alexander_eismont@mail.ru>
2019-11-05 00:10:15 +0100

    solve LGS

--- a1/cagd/utils.py
+++ a1/cagd/utils.py
@@ -53,5 +53,45 @@ def solve_tridiagonal_equation_one_coordinate(diag1, diag2, diag3, res):
 #diag1[i], diag2[i] and diag3[i] are located on the same row of A
 def solve_almost_tridiagonal_equation(diag1, diag2, diag3, res):
     assert(len(diag1) == len(diag2) == len(diag3) == len(res))
-    pass
 
+    res_x = [el.x for el in res]
+    res_y = [el.y for el in res]
+    sol_x = solve_almost_tridiagonal_equation_one_coordinate(diag1, diag2, diag3, res_x)
+    sol_y = solve_almost_tridiagonal_equation_one_coordinate(diag1, diag2, diag3, res_y)
+    return [vec2(sol_x[i], sol_y[i]) for i in range(len(res))]
+
+
+def solve_almost_tridiagonal_equation_one_coordinate(diag1, diag2, diag3, res):
+    n = len(diag2)
+    y = [0] * n
+    v = [0] * n
+    s = [0] * n
+    a = diag1
+    z = [0] * n
+    c = diag3
+    b = diag2
+    d = res
+
+    z[0] = 1 / b[0]
+    v[0] = -z[0] * c[0]
+    y[0] = z[0] * d[0]
+    s[0] = -a[0] * z[0]
+    for i in range(1, n - 1):
+        z[i] = 1 / (b[i] + a[i] * v[i - 1])
+        v[i] = -z[i] * c[i]
+        y[i] = z[i] * (d[i] - a[i] * y[i - 1])
+        s[i] = -a[i] * s[i - 1] * z[i]
+
+    t = [0] * n
+    w = [0] * n
+    t[n - 1] = 1
+    for i in range(n - 2, -1, -1):
+        t[i] = v[i] * t[i + 1] + s[i]
+        w[i] = v[i] * w[i + 1] + y[i]
+
+    x = [0] * n
+    x[n - 1] = (d[n - 1] - a[n - 1] * w[n - 2] - c[n - 1] * w[0]) / (c[n - 1] * t[0] + a[n - 1] * t[n - 2] + b[n - 1])
+    for i in range(n - 2, -1, -1):
+        x[i] = t[i] * x[n - 1] + w[i]
+
+    return x
